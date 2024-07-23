fb61eaac29f4c86f3400f203560a440b4d7da85e
koreanpro22 <dhskim22@gmail.com>
2024-07-23 15:32:29 -0400

    install next

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Portfolio is currently underway! Just testing the deployment</h1>
+    </main>
+  );
+}
